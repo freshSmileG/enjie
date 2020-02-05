0152636b6d61d052ce7f2f2a9fa867ea7d769542
enjie-li <1151451224@qq.com>
2020-02-05 15:06:01 +0800

    使用redisson实现分布式锁

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import org.redisson.Redisson;
+import org.redisson.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -11,4 +14,11 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Bean
+    public Redisson redisson(){
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://localhost:6379").setDatabase(0);
+
+        return (Redisson) Redisson.create(config);
+    }
 }
--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 import com.example.demo.bean.Test;
 import com.example.demo.service.serviceImpl.TestServiceImpl;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
@@ -9,18 +11,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
+import com.example.demo.bean.Test;
+import com.example.demo.service.serviceImpl.TestServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 @Controller
 @ResponseBody
 public class TestController {
     @Autowired
+    Redisson redisson;
+    @Autowired
     TestServiceImpl testServiceImpl;
     //加入redis做缓存
+    @Autowired
+    private StringRedisTemplate stringredisTemplate;
     @Autowired
     private RedisTemplate redisTemplate;
     @RequestMapping("getAll")
     public List<Test> selectAll(){
         List<Test> list = testServiceImpl.selectAll();
-    return list;
+        return list;
     }
 
     @RequestMapping("getById")
@@ -37,4 +54,32 @@ public class TestController {
             return t;
         }
     }
+
+    //实现分布式锁
+    public boolean get(){
+        String lock = "lock";
+        RLock lock1 = redisson.getLock(lock);
+        //
+        try {
+            lock1.lock();//加上redis锁
+            int count = Integer.parseInt(stringredisTemplate.opsForValue().get("count"));
+            if (count>0) {
+                count = count - 1;
+                redisTemplate.opsForValue().set("count", count);
+                stringredisTemplate.delete(lock);
+                return true;
+            }
+            else return false;
+        }
+        finally {
+            //释放redis锁
+            lock1.unlock();
+        }
+
+
+    }
+
+
+
+
 }
